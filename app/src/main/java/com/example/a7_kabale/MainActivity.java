b19d3fb46dd7b389b98e77639b3b6bafae8aa50a
brian <s195456@student.dtu.dk>
2021-03-24 09:49:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a7_kabale/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a7_kabale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (!OpenCVLoader.initDebug())
+            Log.e("OpenCv", "Unable to load OpenCV");
+        else
+            Log.d("OpenCv", "OpenCV loaded");
+
+
+
+    }
+}
