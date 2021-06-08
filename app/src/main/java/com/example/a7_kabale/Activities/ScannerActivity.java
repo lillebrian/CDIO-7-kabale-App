fe10699956154db091e216e688b352d62227bdb0
Mjohansen1997 <mjohansen1997@gmail.com>
2021-06-08 22:55:40 +0200

    added an UI element in shape of cards to show which cards have already been scanned, which element you are about to scan, and by clicking on the UI elements you can remove the scanned card and rescan a new in case of a wrongly scanned card or scanned in the wrong order + added comments to the code

--- app/src/main/java/com/example/a7_kabale/Activities/ScannerActivity.java
+++ app/src/main/java/com/example/a7_kabale/Activities/ScannerActivity.java
@@ -8,9 +8,10 @@ import android.content.res.AssetManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.a7_kabale.R;
@@ -25,7 +26,6 @@ import org.opencv.core.MatOfFloat;
 import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfRect2d;
 import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.Rect2d;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -39,30 +39,43 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-
-import static java.lang.Math.round;
-import static org.opencv.core.Core.FILLED;
-import static org.opencv.imgproc.Imgproc.putText;
-import static org.opencv.imgproc.Imgproc.rectangle;
-
 
 public class ScannerActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
     private static final String TAG = "MainActivity";
+
+    //defines list to contains scannedCards, their confidentiality levels and a list over all the
+    // card names ordered in the same way as the class id's
+    ArrayList<String> scannedCards = new ArrayList<>();
+    ArrayList<Float> scannedCardsConf = new ArrayList<>();
+    List<ImageView> cardViews = new ArrayList<>();
+    List<String> cardNames = Arrays.asList("Ah","Kh","Qh","Jh","10h","9h","8h","7h","6h","5h","4h",
+            "3h","2h","Ad","Kd","Qd","Jd","10d","9d","8d","7d","6d","5d","4d","3d","2d",
+            "Ac","Kc","Qc","Jc","10c","9c","8c","7c","6c","5c","4c","3c","2c","As","Ks",
+            "Qs","Js","10s","9s","8s","7s","6s","5s","4s","3s","2s");
+
+    //linked list to act as queue for which cards that still needs scanning
+    LinkedList<Integer> queue = new LinkedList<>();
+
+    //int value to define how many cards there should be scanned
+    int cardsToScan = 7;
+
+    //classes used for OpenCV
     CameraBridgeViewBase cameraBridgeViewBase;
     BaseLoaderCallback baseLoaderCallback;
+
+    //Boolean values used for running object detection is to be replaced
     boolean startYolo = false;
     boolean firstTimeYolo = false;
     Net tinyYolo;
+
 
     public void Yolo(View button){
         startYolo = !startYolo;
         if (!firstTimeYolo) {
 //            String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg" ;
 //            String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
-
-//              String tinyYoloCfg = getAssetsFile("yolov3.cfg", this);
-//              String tinyYoloWeights = getAssetsFile("yolov3.weights", this);
             String tinyYoloCfg = getAssetsFile("yolov3-tiny-obj-test.cfg", this);
             String tinyYoloWeights = getAssetsFile("yolov3-tiny-obj_10000.weights", this);
             tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
@@ -75,6 +88,39 @@ public class ScannerActivity extends AppCompatActivity implements CameraBridgeVi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+        LinearLayout cardHolder = findViewById(R.id.cardViewer);
+//        RelativeLayout cardHolder = findViewById(R.id.cardViewer);
+        for (int i = 0; i < cardsToScan; i++) {
+            ImageView image = new ImageView(getApplicationContext());
+            image.setContentDescription(String.valueOf(i));
+            if (i == 0)
+                image.setImageDrawable(getDrawable(R.drawable.cards_back_red));
+            else
+                image.setImageDrawable(getDrawable(R.drawable.cards_back));
+            image.setAdjustViewBounds(true);
+            image.setClickable(true);
+            image.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int index = Integer.parseInt(String.valueOf(v.getContentDescription()));
+                    if (queue.contains(index))
+                        return;
+                    ((ImageView) v).setImageResource(R.drawable.cards_back_red);
+                    cardViews.get(queue.peekFirst()).setImageResource(R.drawable.cards_back);
+                    scannedCards.set(index, "");
+                    queue.addFirst(index);
+                }
+            });
+//          Initializes all the different list's that need to contain data
+            cardViews.add(image);
+            cardHolder.addView(image);
+            queue.add(i);
+            scannedCards.add("");
+            //Uncomment if you wanna check the confidentiality things get through with
+            scannedCardsConf.add(0.0f);
+
+        }
 
         cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.cameraView);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
@@ -105,10 +151,14 @@ public class ScannerActivity extends AppCompatActivity implements CameraBridgeVi
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
         Mat frame = inputFrame.rgba();
+        //#TODO replace this with getFirst on the queue to check if empty
         if (startYolo) {
+//        if (queue.peekFirst() == null) {
             Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
-            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416, 416), new Scalar(0, 0, 0), false, false);
-//            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(608, 608), new Scalar(0, 0, 0), false, false);
+
+            //#TODO check which one seems best (Size)
+//            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416, 416), new Scalar(0, 0, 0), false, false);
+            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(608, 608), new Scalar(0, 0, 0), false, false);
 
             tinyYolo.setInput(imageBlob);
 
@@ -120,8 +170,11 @@ public class ScannerActivity extends AppCompatActivity implements CameraBridgeVi
 
             tinyYolo.forward(result, outBlobNames);
 
-            float confThreshold = 0.3f;
+            //defines what confidentiality lvl is required for the software to allow recognition
+            float confThreshold = 0.9f;
 
+            //defines 3 lists 1 for containing classid's, another to contain confenditiality levels
+            //the last to contain the dimensions for the box to be drawn around the the area
             List<Integer> clsIds = new ArrayList<>();
             List<Float> confs = new ArrayList<>();
             List<Rect2d> rects = new ArrayList<>();
@@ -155,43 +208,51 @@ public class ScannerActivity extends AppCompatActivity implements CameraBridgeVi
             int ArrayLength = confs.size();
 
             if (ArrayLength >=1 ) {
-                float nmsThresh = 0.2f;
+                float nmsThresh = 0.6f;
 
                 MatOfFloat confidences = new MatOfFloat(Converters.vector_float_to_Mat(confs));
                 Rect2d[] boxesArray = rects.toArray(new Rect2d[0]);
-//                MatOfRect2d boxes = new MatOfRect2d();
-//                boxes.fromList(boxesArray);
                 MatOfRect2d boxes = new MatOfRect2d(boxesArray);
                 MatOfInt indices = new MatOfInt();
+
+                //performs NMS(Non-Maximum) on the boxes
                 Dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThresh, indices);
 
-                //Draw result boxes
+                //Draws the result boxes
                 int[] ind = indices.toArray();
                 for (int i = 0; i < ind.length; ++i) {
-                    int idx = ind[i];
-                    Rect2d box = boxesArray[idx];
-                    int idGuy = clsIds.get(idx);
-                    float conf = confs.get(idx);
-
-
-                    List<String> cardNames = Arrays.asList("Ah","Kh","Qh","Jh","10h","9h","8h","7h","6h","5h","4h",
-                            "3h","2h","Ad","Kd","Qd","Jd","10d","9d","8d","7d","6d","5d","4d","3d","2d",
-                            "Ac","Kc","Qc","Jc","10c","9c","8c","7c","6c","5c","4c","3c","2c","As","Ks",
-                            "Qs","Js","10s","9s","8s","7s","6s","5s","4s","3s","2s");
-                    List<String> cocoNames = Arrays.asList("a person", "a bicycle", "a motorbike", "an airplane", "a bus", "a train", "a truck", "a boat", "a traffic light", "a fire hydrant", "a stop sign", "a parking meter", "a car", "a bench", "a bird", "a cat", "a dog", "a horse", "a sheep", "a cow", "an elephant", "a bear", "a zebra", "a giraffe", "a backpack", "an umbrella", "a handbag", "a tie", "a suitcase", "a frisbee", "skis", "a snowboard", "a sports ball", "a kite", "a baseball bat", "a baseball glove", "a skateboard", "a surfboard", "a tennis racket", "a bottle", "a wine glass", "a cup", "a fork", "a knife", "a spoon", "a bowl", "a banana", "an apple", "a sandwich", "an orange", "broccoli", "a carrot", "a hot dog", "a pizza", "a doughnut", "a cake", "a chair", "a sofa", "a potted plant", "a bed", "a dining table", "a toilet", "a TV monitor", "a laptop", "a computer mouse", "a remote control", "a keyboard", "a cell phone", "a microwave", "an oven", "a toaster", "a sink", "a refrigerator", "a book", "a clock", "a vase", "a pair of scissors", "a teddy bear", "a hair drier", "a toothbrush");
-
+                    int index = ind[i];
+                    //gets the measurements of the recognized area
+                    Rect2d box = boxesArray[index];
+                    //gets the names of the recognized card from it's class id.
+                    String name = cardNames.get(clsIds.get(index));
+                    //gets the confidentiality lvl of the recognized class/card
+                    float conf = confs.get(index);
+                    //converts the float value of the confidentiality to an integer
                     int intConf = (int) (conf * 100);
-
-//                    Imgproc.putText(frame,cocoNames.get(idGuy) + " " + intConf + "%",box.tl(), Imgproc.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255,255,0),2);
-//                    Imgproc.putText(frame,cardNames.get(idGuy) + " " + intConf + "%",box.tl(), Imgproc.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255,255,0),2);
-                    Imgproc.putText(frame,cardNames.get(idGuy) + " " + intConf + "%",box.tl(), Imgproc.FONT_HERSHEY_SIMPLEX, 2, new Scalar(0,0,255),2);
+                    //Draws the name of the card around the recognized area's
+                    Imgproc.putText(frame,name + " " + intConf + "%",box.tl(), Imgproc.FONT_HERSHEY_SIMPLEX, 2, new Scalar(0,0,255),2);
+                    //Draws an rectangle around the recognized area
                     Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 2);
+                    //check if the recognized card is a already recognized card
+                    if (!scannedCards.contains(name)) {
+                        //removes the index from the queue
+                        int queueIndex = queue.poll();
+                        //inserts the card name into the list of scanned cards
+                        scannedCards.set(queueIndex, name);
+                        //can be used for testing to see how high fails get through in confidentiality
+                        scannedCardsConf.set(queueIndex, conf);
+                        //updates the card on the ui
+                        updateCard(name, queueIndex);
+                    }
+                    //for testing purposes
+                    System.out.println("queue: " + queue.toString());
+                    System.out.println("cards: " + scannedCards.toString());
+                    System.out.println("Conf: " + scannedCardsConf.toString());
                 }
             }
 
         }
-
-
         return frame;
     }
 
@@ -222,7 +283,6 @@ public class ScannerActivity extends AppCompatActivity implements CameraBridgeVi
     protected void onPause() {
         super.onPause();
         if(cameraBridgeViewBase!=null){
-
             cameraBridgeViewBase.disableView();
         }
 
@@ -236,6 +296,19 @@ public class ScannerActivity extends AppCompatActivity implements CameraBridgeVi
         }
     }
 
+    //Method that updates the picture associated with the top cards
+    private void updateCard(String name, int index) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                cardViews.get(index).setImageResource(getResources().getIdentifier("cards_"+name.toLowerCase(), "drawable", getPackageName()));
+                cardViews.get(queue.peekFirst()).setImageResource(R.drawable.cards_back_red);
+            }
+        });
+
+    }
+
+    //method that can load in assets files.
     private static String getAssetsFile(String file, Context context) {
         AssetManager assetManager = context.getAssets();
         BufferedInputStream inputStream;
