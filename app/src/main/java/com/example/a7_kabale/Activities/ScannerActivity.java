fe29c18ead0a0fa6cb7f66032d9cf18472e049b6
Janipai <s195477@student.dtu.dk>
2021-06-22 14:39:55 +0200

    ask for camera permission

--- app/src/main/java/com/example/a7_kabale/Activities/ScannerActivity.java
+++ app/src/main/java/com/example/a7_kabale/Activities/ScannerActivity.java
@@ -4,10 +4,14 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Matrix;
 import android.os.Build;
@@ -112,6 +116,17 @@ public class ScannerActivity extends AppCompatActivity implements CameraBridgeVi
         constraintSet.clone(constraintLayout);
         int cardsToScan = getIntent().getIntExtra("amount", 0);
 //        RelativeLayout cardHolder = findViewById(R.id.cardViewer);
+
+        //request for camera permission
+        if(ContextCompat.checkSelfPermission(ScannerActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            //directly ask for the permissions
+            ActivityCompat.requestPermissions(ScannerActivity.this,new  String[]{
+                    Manifest.permission.CAMERA
+            },100);
+        }
+
+
+
         for (int i = 0; i < cardsToScan; i++) {
             ImageView image = new ImageView(getApplicationContext());
             image.setContentDescription(String.valueOf(i));
@@ -402,4 +417,5 @@ public class ScannerActivity extends AppCompatActivity implements CameraBridgeVi
         setResult(this.RESULT_OK, returnIntent);
         finish();
     }
+
 }
